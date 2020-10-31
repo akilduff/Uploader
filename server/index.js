3210485482ae4cf849bc00ea4fad2a9c4a29a876
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-10-31 10:39:58 -0700

    Serve static file

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-
+app.use('/', express.static(path.join(__dirname, '../dist')))
 
 app.get('/', function (req, res) {
+  res.send('Hello World');
+})
+
+app.post('/uploader', function (req, res) {
   res.send('Hello World');
 })
 
