cbb705d1422b09b9a6d48d1413889d7c2789b11d
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-10-31 10:24:12 -0700

    Prelim react app

--- client/index.jsx
+++ client/index.jsx
@@ -5,14 +5,34 @@ import axios from 'axios';
 class UploaderApp extends React.Component {
   constructor() {
     super ();
-
     this.state = {
-    }
+      name: ''
+    };
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({name: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
   }
 
   render() {
-
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.name} onChange={this.handleChange}/>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    )
   }
 }
 ReactDOM.render(<UploaderApp />, document.getElementById('app'))
