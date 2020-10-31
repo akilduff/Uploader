47f47902ff80ed42fd1863e1c94fcd0773dc4cc8
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-10-31 09:57:14 -0700

    Configuration of webpack with placeholder for entry

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  // entry: path.join(__dirname + "/client/index.jsx"),
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+};
