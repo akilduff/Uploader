a6c2e7b9d85365521d54977d31ca56b8c93c2c3f
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-10-31 10:12:24 -0700

    Create React element and connect to html

---
+++ client/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class UploaderApp extends React.Component {
+  constructor() {
+    super ();
+
+    this.state = {
+    }
+
+  }
+
+  render() {
+
+  }
+}
+ReactDOM.render(<UploaderApp />, document.getElementById('app'))
